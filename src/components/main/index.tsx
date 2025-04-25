fb2028243ec3f34b2f7d26e587c2129b02092a8f
Sousa Morais <vi.morais@yahoo.com>
2025-04-24 22:23:35 -0300

    :fire: remove hero image

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,9 +1,12 @@
-import heroImage from "../../../public/Hero Image.jpg"
 
 export function Main() {
   return (
     <main className="flex flex-col items-center">
-      <img className="max-w-[70rem] mt-10" src={heroImage} alt="Imagem principal" />
+      <section className="max-w-96 h-20">
+        <div>
+          <img src="" alt="Marca da Clínica Veterinária" />
+        </div>
+      </section>
     </main>
-  )
+  );
 }
