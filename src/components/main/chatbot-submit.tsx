d1f312ce992c3db3e27074184161e755da41329e
Sousasz <vi.morais@yahoo.com>
2025-06-09 09:33:45 -0300

    :fire: remove code from chatbot

--- src/components/main/chatbot-submit.tsx
+++ src/components/main/chatbot-submit.tsx
@@ -1,31 +1,31 @@
 import { FaRegPaperPlane } from "react-icons/fa";
-import { FormEvent, useState } from "react";
+import { FormEvent, useState } from 'react'
 
 export function ChatBotSubmit() {
 
-  const [questions, setQuestions] = useState<string[] | undefined>([]);
+  const [message, setMessage] = useState<string | undefined>()
 
-  function addQuestion(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-
-    const target = new FormData(event.currentTarget)
+  function addQuestion(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const target = new FormData(e.currentTarget)
     const value = target.get("question")?.toString()
 
-    setQuestions([...questions, value])
-    console.log(questions)
+    setMessage(value)
+
+    console.log(message)
   }
 
   return (
-    <div className="w-full flex border-2 border-t-green-light border-b-white p-4">
+    <form onSubmit={() => addQuestion} className="w-full flex border-2 border-t-green-light border-b-white p-4">
       <input
         name="question"
         className="w-full h-full text-black outline-none"
         type="text"
         placeholder="Digite aqui sua mensagem..."
       />
-      <button onSubmit={() => addQuestion} className="cursor-pointer">
+      <button className="cursor-pointer">
         <FaRegPaperPlane className="size-5" />
       </button>
-    </div>
+    </form>
   );
 }
