6e32b9bddadc6d83dd30ec2841059fa168397691
Sousasz <vi.morais@yahoo.com>
2025-06-17 08:42:09 -0300

    :lipstick: add all areas for scheduling modal

--- src/components/modals/scheduling-modal/index.tsx
+++ src/components/modals/scheduling-modal/index.tsx
@@ -17,9 +17,9 @@ export function SchedulingModal() {
         </DialogTrigger>
 
         <DialogContent className="sm:max-w-[90%] shadow-default h-[90%] bg-green-light">
-          <div className="backdrop-blur-md bg-white/25 shadow-2xl p-10 rounded-lg flex flex-col">
-            <DialogHeader>
-              <DialogTitle className="text-center font-poppins">
+          <div className="backdrop-blur-md bg-white/25 shadow-2xl p-10 rounded-lg flex flex-col gap-10">
+            <DialogHeader className="max-w-full flex items-center">
+              <DialogTitle className="text-center text-3xl font-poppins font-light p-1 w-80">
                 AGENDAMENTO
               </DialogTitle>
             </DialogHeader>
--- src/components/shared/title/index.tsx
+++ src/components/shared/title/index.tsx
@@ -1,4 +1,4 @@
-import TitleAsset from "../../../../public/assets/title-asset.svg?react";
+// import TitleAsset from "../../../../public/assets/title-asset.svg?react";
 
 type TitleProps = {
   textInBold?: string;
@@ -8,12 +8,15 @@ type TitleProps = {
 
 export function Title({ textInBold, text, className }: TitleProps) {
   return (
-    <h3 className={`text-2xl relative ${className}`}>
-      <span className="font-bold text-green-light ">
-        {textInBold}
-      </span>{" "}
-      {text}
-      {/* <TitleAsset className="relative" /> */}
-    </h3>
+    <div className="max-w-full flex justify-center">
+      <h3 className={`text-2xl relative text-center bg-white px-5 p-1 w-fit h-fit shadow-md ${className}`}>
+        <span className="font-bold text-green-light ">
+          {textInBold}
+        </span>{" "}
+        {text}
+        
+        {/* <TitleAsset className="relative" /> */}
+      </h3>
+    </div>
   );
 }
